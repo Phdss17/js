c2831dcf182cd78ee4a3055a25de67be76e51405
Ph <phenriquedss@alu.ufc.br>
2025-08-04 23:24:41 -0300

    atividade 1 - criação de modelo e site completo usando as tecnologias

---
+++ ex001/js/script.js
@@ -0,0 +1,35 @@
+function carregar() {
+    let msg = document.getElementById('msg');
+    let img = document.getElementById('imagem');
+    let saud = document.getElementById('saudacao');
+    let data = new Date();
+    let hora = data.getHours();
+    let minutos = data.getMinutes();
+    hora = 13;
+    msg.innerHTML = `Agora são ${hora}:${minutos}`;
+    if(hora < 12 && hora >= 5){
+        amanhecer(img, saud);
+    }else if(hora >= 12 && hora < 18){
+        entardecer(img, saud);
+    }else{
+        anoitecer(img, saud);
+    }
+}
+
+function amanhecer(img, saud) {
+    img.src = 'assets/morningimage.jpg';
+    document.body.style.background = "#ADD8E6";
+    saud.innerHTML = 'Bom dia!';
+}
+
+function entardecer(img, saud) {
+    img.src = 'assets/afternoonimage.jpg';
+    document.body.style.background = '#3a3a91ff';
+    saud.innerHTML = 'Boa tarde!';
+}
+
+function anoitecer(img, saud) {
+    img.src = 'assets/nightimage.jpg'
+    document.body.style.background = '#2a293e ';
+    saud.innerHTML = 'Boa noite!';
+}
